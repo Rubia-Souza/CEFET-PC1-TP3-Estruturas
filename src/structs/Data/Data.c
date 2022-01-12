2c39411cdfaa5b8fcd0437a9ba044478fd1506fb
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-01-12 14:49:49 -0300

    feat(structs): Cria strcuct de Data

---
+++ src/structs/Data/Data.c
@@ -0,0 +1,16 @@
+#include "Data.h"
+
+void setDia(Data *data, const unsigned int dia) {
+    data->dia = dia;
+    return;
+}
+
+void setMes(Data *data, const unsigned int mes) {
+    data->mes = mes;
+    return;
+}
+
+void setAno(Data *data, const unsigned int ano) {
+    data->ano = ano;
+    return;
+}
---
+++ src/structs/Data/Data.h
@@ -0,0 +1,9 @@
+typedef struct Data {
+    unsigned int dia;
+    unsigned int mes;
+    unsigned int ano;
+} Data;
+
+void setDia(Data *data, const unsigned int dia);
+void setMes(Data *data, const unsigned int mes);
+void setAno(Data *data, const unsigned int ano);
