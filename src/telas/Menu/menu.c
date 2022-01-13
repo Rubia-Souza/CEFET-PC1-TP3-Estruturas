517e4390fc9c476fa382feb7eebdbba63af34ac3
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-01-13 18:40:12 -0300

    feat(telas): Implementa tela de menu

---
+++ src/telas/Menu/menu.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "./menu.h"
+
+#define OPCAO_MENU_MINIMA 1
+#define OPCAO_MENU_MAXIMA 7
+
+void renderizarOpcoes();
+unsigned int getOpcao();
+
+unsigned int renderizarMenu() {
+    printf("===== Rúbia Alice - Produtos =====\n\n");
+    
+    renderizarOpcoes();
+    unsigned int escolha = getOpcao();
+
+    return escolha;
+}
+
+void renderizarOpcoes() {
+    printf("[1] Adicionar produto\n");
+    printf("[2] Exibir produtos\n");
+    printf("[3] Exibir produto mais caro\n");
+    printf("[4] Exibir produto mais barato\n");
+    printf("[5] Excluir produto\n");
+    printf("[6] Ordernar produtos\n");
+    printf("[7] Sair\n");
+    printf("\n");
+
+    return;
+}
+
+unsigned int getOpcao() {
+    unsigned int opcao = 0;
+    bool inputErrado = false;
+
+    printf("\nEscolha uma das opções acima: ");
+    do {
+        scanf("%d", &opcao);
+        fflush(stdin);
+
+        inputErrado = opcao < OPCAO_MENU_MINIMA || opcao > OPCAO_MENU_MAXIMA;
+        if(inputErrado) {
+            printf("\nPor favor, escolha uma opção válida (de 1 a 6): ");
+        }
+    } while(inputErrado);
+    
+    return opcao;
+}
---
+++ src/telas/Menu/menu.h
@@ -0,0 +1,16 @@
+#ifndef MENU_H
+#define MENU_H
+
+enum OpcoesMenu {
+    ADICIONAR = 1,
+    EXIBIR_TODOS_PRODUTOS = 2,
+    EXIBIR_PRODUTO_CARO = 3,
+    EXIBIR_PRODUTO_BRARATO = 4,
+    EXCLUIR_PRODUTO = 5,
+    ORDENAR_PRODUTOS = 6,
+    SAIR = 7,
+} OpcoesMenu;
+
+unsigned int renderizarMenu();
+
+#endif
