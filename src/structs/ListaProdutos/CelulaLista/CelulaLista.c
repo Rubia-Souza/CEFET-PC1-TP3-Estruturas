bbfa2cffb0cbf16b0a4569cf017fb631ee61204b
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-01-13 11:36:06 -0300

    feat(structs): Implementa alocação da celula

--- src/structs/ListaProdutos/CelulaLista/CelulaLista.c
+++ src/structs/ListaProdutos/CelulaLista/CelulaLista.c
@@ -1,9 +1,18 @@
+#include <stdlib.h>
 #include "./CelulaListaStruct.h"
 
 Celula* newCelula(){
-    // TODO: Implementar
+    Celula* novaCelula = (Celula*) malloc(sizeof(Celula));
+
+    novaCelula->anterior = NULL;
+    novaCelula->proxima = NULL;
+
+    return novaCelula;
 }
 
 Celula* newCelulaComValor(const Produto produto){
-    // TODO: Implementar
+    Celula* novaCelula = newCelula();
+    novaCelula->produto = produto;
+
+    return novaCelula;
 }
