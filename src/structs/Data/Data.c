1a2d75b28594c5b59ed2fd58e69ef7675af574c4
Rubia Souza <rubensvictor.m.s@gmail.com>
2022-01-13 11:20:07 -0300

    refactor(struct): Ajusta referências de importar Data

--- src/structs/Data/Data.c
+++ src/structs/Data/Data.c
@@ -1,16 +1,16 @@
-#include "Data.h"
+#include "DataStruct.h"
 
-void setDia(Data *data, const unsigned int dia) {
+void setDia(Data* data, const unsigned int dia) {
     data->dia = dia;
     return;
 }
 
-void setMes(Data *data, const unsigned int mes) {
+void setMes(Data* data, const unsigned int mes) {
     data->mes = mes;
     return;
 }
 
-void setAno(Data *data, const unsigned int ano) {
+void setAno(Data* data, const unsigned int ano) {
     data->ano = ano;
     return;
 }
---
+++ src/structs/Data/DataStruct.h
@@ -0,0 +1,14 @@
+#ifndef DATA_H
+#define DATA_H
+
+typedef struct Data {
+    unsigned int dia;
+    unsigned int mes;
+    unsigned int ano;
+} Data;
+
+void setDia(Data* data, const unsigned int dia);
+void setMes(Data* data, const unsigned int mes);
+void setAno(Data* data, const unsigned int ano);
+
+#endif
